352cff3fca0958856a371d0438cd66bc97e00cf7
bawiga <bawigarogine02@gmail.com>
2025-04-01 14:44:01 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tourist-guide/', // Must match your repo name exactly
+  base: '/tourist-guide/',
+  build: {
+    outDir: 'docs'
+  }
 })
