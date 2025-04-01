f640c49bc3d6470641e08c13b46504535a17e0df
bawiga <bawigarogine02@gmail.com>
2025-04-01 19:04:19 +0800

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-import markerIcon from "../node_modules/leaflet/dist/images/marker-icon.png";
-L.Marker.prototype.setIcon(L.icon({
-  iconUrl:markerIcon
-}))
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
